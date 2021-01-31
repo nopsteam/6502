be53e8856c6bf900b5838d7144feb34f8082d195
Rafael Delboni <rafadelboni@gmail.com>
2021-01-31 12:51:37 -0300

    feat: Rendering 4 red pixels moved by keys

--- main.c
+++ main.c
@@ -1,132 +1,177 @@
-#include <SDL2/SDL.h> 
-#include <SDL2/SDL_image.h> 
-#include <SDL2/SDL_timer.h> 
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_rect.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_timer.h>
 
-int main() 
-{ 
+struct State
+{
+  int key_quit;
+  int key_up;
+  int key_left;
+  int key_right;
+  int key_down;
+};
 
-  // retutns zero on success else non-zero 
-  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) { 
-    printf("error initializing SDL: %s\n", SDL_GetError()); 
-  } 
-  SDL_Window* win = SDL_CreateWindow("GAME", // creates a window 
-      SDL_WINDOWPOS_CENTERED, 
-      SDL_WINDOWPOS_CENTERED, 
+struct Point
+{
+  int x;
+  int y;
+};
+
+SDL_Window* init_window()
+{
+  // returns zero on success else non-zero
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+    printf("error initializing SDL: %s\n", SDL_GetError());
+  }
+
+  return SDL_CreateWindow("6502",
+      SDL_WINDOWPOS_CENTERED,
+      SDL_WINDOWPOS_CENTERED,
       1000, 1000, 0); 
+}
 
-  // triggers the program that controls 
-  // your graphics hardware and sets flags 
-  Uint32 render_flags = SDL_RENDERER_ACCELERATED; 
+struct State event_manager(struct State state) {
+  struct State local_state = state;
+  SDL_Event event;
 
-  // creates a renderer to render our images 
-  SDL_Renderer* rend = SDL_CreateRenderer(win, -1, render_flags); 
+  while (SDL_PollEvent(&event)) {
 
-  // creates a surface to load an image into the main memory 
-  SDL_Surface* surface; 
+    if (event.type == SDL_QUIT)
+      local_state.key_quit = 1;
 
-  // please provide a path for your image 
-  surface = IMG_Load("path");
+    if (event.type == SDL_KEYDOWN)
+    {
+      switch (event.key.keysym.scancode) {
+        case SDL_SCANCODE_W:
+        case SDL_SCANCODE_UP:
+          local_state.key_up = 1;
+          break;
+        case SDL_SCANCODE_A:
+        case SDL_SCANCODE_LEFT:
+          local_state.key_left = 1;
+          break;
+        case SDL_SCANCODE_S:
+        case SDL_SCANCODE_DOWN:
+          local_state.key_down = 1;
+          break;
+        case SDL_SCANCODE_D:
+        case SDL_SCANCODE_RIGHT:
+          local_state.key_right = 1;
+          break;
+        default:
+          break;
+      }
+    }
+    if (event.type == SDL_KEYUP)
+    {
+      switch (event.key.keysym.scancode) {
+        case SDL_SCANCODE_W:
+        case SDL_SCANCODE_UP:
+          local_state.key_up = 0;
+          break;
+        case SDL_SCANCODE_A:
+        case SDL_SCANCODE_LEFT:
+          local_state.key_left = 0;
+          break;
+        case SDL_SCANCODE_S:
+        case SDL_SCANCODE_DOWN:
+          local_state.key_down = 0;
+          break;
+        case SDL_SCANCODE_D:
+        case SDL_SCANCODE_RIGHT:
+          local_state.key_right = 0;
+          break;
+        default:
+          break;
+      }
+    }
+  }
+  return local_state;
+}
 
-  // loads image to our graphics hardware memory. 
-  SDL_Texture* tex = SDL_CreateTextureFromSurface(rend, surface); 
+void loop(SDL_Renderer* rend)
+{
+  struct State local_state;
+  local_state.key_quit = 0;
+  local_state.key_up = 0;
+  local_state.key_left = 0;
+  local_state.key_right = 0;
+  local_state.key_down = 0;
 
-  // clears main-memory 
-  SDL_FreeSurface(surface); 
+  struct Point pixel;
+  pixel.x = 500;
+  pixel.y = 500;
 
-  // let us control our image position 
-  // so that we can move it with our keyboard. 
-  SDL_Rect dest; 
+  while (!local_state.key_quit) {
+    // read input save in state
+    local_state = event_manager(local_state);
 
-  // connects our texture with dest to control position 
-  SDL_QueryTexture(tex, NULL, NULL, &dest.w, &dest.h); 
+    // speed
+    int speed = 300;
 
-  // adjust height and width of our image box. 
-  dest.w /= 6; 
-  dest.h /= 6; 
+    // set render color black
+    SDL_SetRenderDrawColor(rend, 0, 0, 0, 0);
+    // clears the screen
+    SDL_RenderClear(rend);
 
-  // sets initial x-position of object 
-  dest.x = (1000 - dest.w) / 2; 
+    // move pixel
+    if (local_state.key_up)
+      pixel.y -= speed / 30;
+    if (local_state.key_left)
+      pixel.x -= speed / 30;
+    if (local_state.key_down)
+      pixel.y += speed / 30;
+    if (local_state.key_right)
+      pixel.x += speed / 30;
 
-  // sets initial y-position of object 
-  dest.y = (1000 - dest.h) / 2; 
+    // right boundary
+    if (pixel.x + 1 > 1000)
+      pixel.x = 1000 - 1;
 
-  // controls annimation loop 
-  int close = 0; 
+    // left boundary
+    if (pixel.x < 0)
+      pixel.x = 0;
 
-  // speed of box 
-  int speed = 300; 
+    // bottom boundary
+    if (pixel.y + 1 > 1000)
+      pixel.y = 1000 - 1;
 
-  // annimation loop 
-  while (!close) { 
-    SDL_Event event; 
+    // upper boundary
+    if (pixel.y < 0)
+      pixel.y = 0;
 
-    // Events mangement 
-    while (SDL_PollEvent(&event)) { 
-      switch (event.type) { 
+    // set render color red
+    SDL_SetRenderDrawColor(rend, 255, 0, 0, 255);
 
-        case SDL_QUIT: 
-          // handling of close button 
-          close = 1; 
-          break; 
+    // render four pixels
+    SDL_RenderDrawPoint(rend, pixel.x, pixel.y);
+    SDL_RenderDrawPoint(rend, pixel.x+1, pixel.y+1);
+    SDL_RenderDrawPoint(rend, pixel.x, pixel.y+1);
+    SDL_RenderDrawPoint(rend, pixel.x+1, pixel.y);
 
-        case SDL_KEYDOWN: 
-          // keyboard API for key pressed 
-          switch (event.key.keysym.scancode) { 
-            case SDL_SCANCODE_W: 
-            case SDL_SCANCODE_UP: 
-              dest.y -= speed / 30; 
-              break; 
-            case SDL_SCANCODE_A: 
-            case SDL_SCANCODE_LEFT: 
-              dest.x -= speed / 30; 
-              break; 
-            case SDL_SCANCODE_S: 
-            case SDL_SCANCODE_DOWN: 
-              dest.y += speed / 30; 
-              break; 
-            case SDL_SCANCODE_D: 
-            case SDL_SCANCODE_RIGHT: 
-              dest.x += speed / 30; 
-              break; 
-          } 
-      } 
-    } 
+    // triggers the double buffers for multiple rendering
+    SDL_RenderPresent(rend);
 
-    // right boundary 
-    if (dest.x + dest.w > 1000) 
-      dest.x = 1000 - dest.w; 
+    // calculates to 60 fps
+    SDL_Delay(1000 / 60);
+  }
 
-    // left boundary 
-    if (dest.x < 0) 
-      dest.x = 0; 
+  return;
+}
 
-    // bottom boundary 
-    if (dest.y + dest.h > 1000) 
-      dest.y = 1000 - dest.h; 
+int main()
+{
+  SDL_Window* win = init_window();
+  Uint32 render_flags = SDL_RENDERER_ACCELERATED;
+  SDL_Renderer* rend = SDL_CreateRenderer(win, -1, render_flags);
 
-    // upper boundary 
-    if (dest.y < 0) 
-      dest.y = 0; 
+  loop(rend);
 
-    // clears the screen 
-    SDL_RenderClear(rend); 
-    SDL_RenderCopy(rend, tex, NULL, &dest); 
-
-    // triggers the double buffers 
-    // for multiple rendering 
-    SDL_RenderPresent(rend); 
-
-    // calculates to 60 fps 
-    SDL_Delay(1000 / 60); 
-  } 
-
-  // destroy texture 
-  SDL_DestroyTexture(tex); 
-
-  // destroy renderer 
-  SDL_DestroyRenderer(rend); 
-
-  // destroy window 
+  SDL_DestroyRenderer(rend);
   SDL_DestroyWindow(win); 
+
   return 0; 
-} 
+}
