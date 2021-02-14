dacda5d79877c14062d9c7f616c78ae72bfa15c7
Rafael Delboni <rafadelboni@gmail.com>
2021-02-14 11:22:53 -0300

    refact: Disassembler using sc string and format

--- app/6502_disasm.c
+++ app/6502_disasm.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "constants.h"
 #include "disassembler.h"
 
 int main(int argc, char **argv)
@@ -14,56 +13,10 @@ int main(int argc, char **argv)
   struct PROGRAM programLoaded = LoadBinary(programPath);
 
   for (int i = 0; i < programLoaded.lines; i++) {
-    char * instruction = programLoaded.program[i].opcode->instruction;
-    char firstOperand = programLoaded.program[i].args[0];
-    char secondOperand = programLoaded.program[i].args[1];
-    int addressMode = programLoaded.program[i].opcode->addressing->index;
-
-    switch(addressMode) {
-      case Implied:
-        printf("%s\n", instruction);
-        break;
-      case Accumulator:
-        printf("%s A\n", instruction);
-        break;
-      case Immediate:
-        printf("%s #$%02x \n", instruction, firstOperand & 0xFF);
-        break;
-      case ZeroPage:
-        printf("%s $%02x \n", instruction, firstOperand & 0xFF);
-        break;
-      case ZeroPageX:
-        printf("%s $%02x,X \n", instruction, firstOperand & 0xFF);
-        break;
-      case ZeroPageY:
-        printf("%s $%02x,Y \n", instruction, firstOperand & 0xFF);
-        break;
-      case Relative:
-        printf("%s 0x%02x \n", instruction, firstOperand & 0xFF);
-        break;
-      case IndirectX:
-        printf("%s ($%02x,X) \n", instruction, firstOperand & 0xFF);
-        break;
-      case IndirectY:
-        printf("%s ($%02x),Y \n", instruction, firstOperand & 0xFF);
-        break;
-      case AbsoluteX:
-        printf("%s $%02x%02x,X \n", instruction, firstOperand & 0xFF, secondOperand & 0xFF);
-        break;
-      case AbsoluteY:
-        printf("%s $%02x%02x,Y \n", instruction, firstOperand & 0xFF, secondOperand & 0xFF);
-        break;
-      case Absolute:
-        printf("%s $%02x%02x \n", instruction, firstOperand & 0xFF, secondOperand & 0xFF);
-        break;
-      case Indirect:
-        printf("%s ($%02x%02x) \n", instruction, firstOperand & 0xFF, secondOperand & 0xFF);
-        break;
-      default:
-        printf("more than 2 operands is not supported");
-    }
+    printf("%s ", toStringHex(&programLoaded.program[i]));
+    printf("%s \n", toStringAsm(&programLoaded.program[i]));
   }
 
   free(programLoaded.program);
-  return 0; 
+  return 0;
 }
