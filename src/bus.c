9f772158156b17068b031cd485d614b54c9af6c6
Rafael Delboni <rafadelboni@gmail.com>
2021-02-24 23:12:01 -0300

    feat: Adds bus layer

---
+++ src/bus.c
@@ -0,0 +1,57 @@
+#include "bus.h"
+
+struct BUS initialize() {
+  struct BUS bus;
+
+  bus.input = 0x00;
+
+  for(int i = 1; i <= 1023; ++i)
+  {
+    bus.display[i] = 0x00;
+  }
+
+  for(int i = 1; i <= (64 * 1024); ++i)
+  {
+    bus.memory[i] = 0x00;
+  }
+
+  return bus;
+}
+
+void write(int addr, char data, struct BUS * bus) {
+  if ((addr >= 0x0000 && addr <  0x00FF) ||
+      (addr >  0x00FF && addr <  0x0200) ||
+      (addr >  0x05FF && addr <= 0xFFFF))
+  {
+    *bus->memory[addr] = data;
+  }
+  else if (addr >= 0x0200 && addr <= 0x05FF)
+  {
+    *bus->display[addr - 0x0200] = data;
+  }
+  else if (addr == 0x00FF)
+  {
+    *bus->input = data;
+  }
+}
+
+char read(int addr, struct BUS * bus) {
+  char data = 0x00;
+
+  if ((addr >= 0x0000 && addr <  0x00FF) ||
+      (addr >  0x00FF && addr <  0x0200) ||
+      (addr >  0x05FF && addr <= 0xFFFF))
+  {
+    data = *bus->memory[addr];
+  }
+  else if (addr >= 0x0200 && addr <= 0x05FF)
+  {
+    data = *bus->display[addr - 0x0200];
+  }
+  else if (addr == 0x00FF)
+  {
+    data = *bus->input;
+  }
+
+  return data;
+}
---
+++ src/bus.h
@@ -0,0 +1,9 @@
+struct BUS {
+    char *input;
+    char *display[1023];
+    char *memory[64 * 1024];
+};
+
+struct BUS initialize();
+void write(int addr, char data, struct BUS * bus);
+char read(int addr, struct BUS * bus);
