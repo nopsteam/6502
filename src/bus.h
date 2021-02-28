29b2e746c711e3a401bd6f6b1472ec19ab2bfc4e
Rafael Delboni <rafadelboni@gmail.com>
2021-02-28 10:42:13 -0300

    test: Adds stazp integration test

--- src/bus.h
+++ src/bus.h
@@ -6,5 +6,5 @@ struct BUS {
 
 struct BUS initializeBus();
 void writeBus(unsigned int addr, unsigned char data, struct BUS * bus);
-char readBus(unsigned int addr, struct BUS * bus);
-void writeFileBus(char * filePath, int offset, struct BUS * bus);
+unsigned char readBus(unsigned int addr, struct BUS * bus);
+void writeFileBus(char * filePath, unsigned int offset, struct BUS * bus);
--- test/cpu_test.c
+++ test/cpu_test.c
@@ -29,12 +29,33 @@ void test_function_should_reset_cpu(void) {
   TEST_ASSERT_EQUAL(false, cpu.status.negative);
 }
 
+void test_cpu_should_sta_zeropage(void) {
+  // STA Zero Page with previusly loaded accumulator register 0xDD
+  /* STA $0d */
+
+  resetCpu(&cpu, &bus);
+  cpu.accumulator = 0xDD; 
+
+  // set STA Zero Page opcode instruction
+  writeBus(0x600, 0x85, &bus);
+  // set 0x0D at next byte to process
+  writeBus(0x601, 0x0D, &bus);
+
+  clockCpu(&cpu, &bus);
+
+  // should move pc 2 times
+  TEST_ASSERT_EQUAL(0x602, cpu.pc);
+  // should store Accumulator register on memory address 0x0D
+  TEST_ASSERT_EQUAL(0xDD, readBus(0x0D, &bus)); 
+}
+
 typedef void (*Func)(void);
 
 int main(void) {
     UNITY_BEGIN();
 
     RUN_TEST(test_function_should_reset_cpu, 1);
+    RUN_TEST(test_cpu_should_sta_zeropage, 2);
 
     return UNITY_END();
 }
