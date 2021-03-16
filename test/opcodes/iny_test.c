5d98586dbf0d0bc6cad01a65c5999937068fec99
Lucas <lucas.ce.bertin@gmail.com>
2021-03-16 20:22:43 -0300

    test: added iny tests

--- test/opcodes/iny_test.c
+++ test/opcodes/iny_test.c
@@ -4,6 +4,8 @@
 struct BUS bus;
 struct CPU cpu;
 
+char INY = 0xc8;
+
 void setUp(void) {
   bus = initializeBus();
   writeBus(0xFFFC, 0x00, &bus);
@@ -14,42 +16,54 @@ void setUp(void) {
 void tearDown(void) {
 }
 
-
 void should_add_1_to_the_Y_register(void) {
-  TEST_ASSERT_EQUAL(0x602, cpu.pc);
+  writeBus(0x600, INY, &bus);
+  cpu.index_y = 0x01;
+  clockCpu(&cpu, &bus);
+  TEST_ASSERT_EQUAL(2, cpu.index_y);
 }
 
-void should_set_zero_flag_when_Y_equals_memory_value(void){
-  TEST_ASSERT_EQUAL(0x602, cpu.pc);
+void should_set_zero_flag_when_Y_equals_zero(void){
+  writeBus(0x600, INY, &bus);
+  cpu.index_y = 0xff;
+  clockCpu(&cpu, &bus);
+  TEST_ASSERT_EQUAL(true, cpu.status.zero);
 }
 
 void should_set_negative_flag_when_bit_7_is_set(void){
-  TEST_ASSERT_EQUAL(0x602, cpu.pc);
-}
-
-void should_not_set_carry_flag_when_Y_less_than_memory_value(void){
-  TEST_ASSERT_EQUAL(0x602, cpu.pc);
+  writeBus(0x600, INY, &bus);
+  cpu.index_y = 0xfd;
+  clockCpu(&cpu, &bus);
+  TEST_ASSERT_EQUAL(true, cpu.status.negative);
 }
 
 void should_not_set_zero_flag_when_Y_more_than_memory_value(void){
-  TEST_ASSERT_EQUAL(0x602, cpu.pc);
+  writeBus(0x600, INY, &bus);
+  cpu.index_y = 0x01;
+  clockCpu(&cpu, &bus);
+  TEST_ASSERT_EQUAL(false, cpu.status.negative);
 }
 
 void should_not_set_zero_flag_when_Y_less_then_memory_value(void){
-  TEST_ASSERT_EQUAL(0x602, cpu.pc);
+  writeBus(0x600, INY, &bus);
+  cpu.index_y = 0xfe;
+  clockCpu(&cpu, &bus);
+  TEST_ASSERT_EQUAL(false, cpu.status.zero);
 }
 
 void should_not_set_negative_flag_when_bit_7_is_not_set(void){
-  TEST_ASSERT_EQUAL(0x602, cpu.pc);
+  writeBus(0x600, INY, &bus);
+  cpu.index_y = 0x01;
+  clockCpu(&cpu, &bus);
+  TEST_ASSERT_EQUAL(false, cpu.status.negative);
 }
 
 int main(void) {
     UNITY_BEGIN();
 
     RUN_TEST(should_add_1_to_the_Y_register);
-    RUN_TEST(should_set_zero_flag_when_Y_equals_memory_value);
+    RUN_TEST(should_set_zero_flag_when_Y_equals_zero);
     RUN_TEST(should_set_negative_flag_when_bit_7_is_set);
-    RUN_TEST(should_not_set_carry_flag_when_Y_less_than_memory_value);
     RUN_TEST(should_not_set_zero_flag_when_Y_more_than_memory_value);
     RUN_TEST(should_not_set_zero_flag_when_Y_less_then_memory_value);
     RUN_TEST(should_not_set_negative_flag_when_bit_7_is_not_set);
