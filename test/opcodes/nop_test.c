66ac339acb20564f55ac74a9463e8f9d84563557
Lucas <lucas.ce.bertin@gmail.com>
2021-03-20 10:48:09 -0300

    test: added nop tests

--- test/opcodes/nop_test.c
+++ test/opcodes/nop_test.c
@@ -15,7 +15,20 @@ void tearDown(void) {
 }
 
 void should_dont_change_anything(void) {
-  TEST_ASSERT_EQUAL(0x602, cpu.pc);
+  writeBus(0x600, 0xea, &bus);
+  clockCpu(&cpu, &bus);
+  TEST_ASSERT_EQUAL(0x601, cpu.pc);
+  TEST_ASSERT_EQUAL(0x00, cpu.index_x);
+  TEST_ASSERT_EQUAL(0x00, cpu.index_y);
+  TEST_ASSERT_EQUAL(0x00, cpu.accumulator);
+  TEST_ASSERT_EQUAL(0xFD, cpu.stack_pointer);
+  TEST_ASSERT_EQUAL(false, cpu.status.break_cmd);
+  TEST_ASSERT_EQUAL(false, cpu.status.carry);
+  TEST_ASSERT_EQUAL(false, cpu.status.decimal);
+  TEST_ASSERT_EQUAL(false, cpu.status.negative);
+  TEST_ASSERT_EQUAL(false, cpu.status.interrupt);
+  TEST_ASSERT_EQUAL(false, cpu.status.overflow);
+  TEST_ASSERT_EQUAL(false, cpu.status.zero);
 }
 
 int main(void) {
