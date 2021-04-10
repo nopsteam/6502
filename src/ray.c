7d87c90ca990d13442c4af3393fc49d42d66f40f
Rafael Delboni <rafadelboni@gmail.com>
2021-04-10 19:17:58 -0300

    fix: FOR THE LOVE OF BILL GATES, work pls

--- src/ray.c
+++ src/ray.c
@@ -1,9 +1,24 @@
 #include "ray.h"
+#include "raylib.h"
 
-/*struct Color colors[16] = {*/
-  /*E_BLACK, E_WHITE, E_RED  , E_CYAN  , E_PINK  , E_GREEN, E_BLUE  , E_YELLOW,*/
-  /*E_BEIGE, E_BROWN, E_PEACH, E_DRGRAY, E_MDGRAY, E_LIME , E_LTBLUE, E_LTGRAY*/
-/*};*/
+struct RayColor colors[16] = {
+  {.r =   0, .g =   0, .b =   0, .a = 255},  //BLACK
+  {.r = 255, .g = 255, .b = 255, .a = 255},  //WHITE
+  {.r = 136, .g =   0, .b =   0, .a = 255},  //RED
+  {.r = 170, .g = 255, .b = 238, .a = 255},  //CYAN
+  {.r = 204, .g =  68, .b = 204, .a = 255},  //PINK
+  {.r =   0, .g = 204, .b =  85, .a = 255},  //GREEN
+  {.r =   0, .g =   0, .b = 170, .a = 255},  //BLUE
+  {.r = 238, .g = 238, .b = 119, .a = 255},  //YELLOW
+  {.r = 221, .g = 136, .b =  85, .a = 255},  //BEIGE
+  {.r = 102, .g =  68, .b =   0, .a = 255},  //BROWN
+  {.r = 255, .g = 119, .b = 119, .a = 255},  //PEACH
+  {.r =  51, .g =  51, .b =  51, .a = 255},  //DRGRAY
+  {.r = 119, .g = 119, .b = 119, .a = 255},  //MDGRAY
+  {.r = 170, .g = 255, .b = 102, .a = 255},  //LIME
+  {.r =   0, .g = 136, .b = 255, .a = 255},  //LTBLUE
+  {.r = 187, .g = 187, .b = 187, .a = 255}   //LTGRAY
+};
 
 int readInput(void) {
   return GetCharPressed();
@@ -26,13 +41,14 @@ void stopWindow(void)
 
 void drawScreen(int scale, unsigned char display[]) {
   BeginDrawing();
-  ClearBackground(E_BLACK);
+  ClearBackground(BLACK);
   for (int i = 0; i <= 1023; i++) {
     float y = floor((float)i / 32);
     float x = i % 32;
-    Color color = E_WHITE; //colors[display[i] & 0x0F];
+    struct RayColor raycolor = colors[display[i] & 0x0F];
+    Color color = {.r = raycolor.r, .g = raycolor.g, .b = raycolor.b, .a = raycolor.a };
     DrawRectangle(x * scale, y * scale, scale, scale, color);
   }
-  DrawText(TextFormat("%2i FPS", GetFPS()), 580, 620, 12, E_LTGRAY);
+  DrawText(TextFormat("%2i FPS", GetFPS()), 580, 620, 12, LIGHTGRAY);
   EndDrawing();
 }
--- src/ray.h
+++ src/ray.h
@@ -1,24 +1,14 @@
 #include <math.h>
 #include "raylib.h"
 
-#define E_BLACK  CLITERAL(Color){  0,   0,   0, 255}
-#define E_WHITE  CLITERAL(Color){255, 255, 255, 255}
-#define E_RED    CLITERAL(Color){136,   0,   0, 255}
-#define E_CYAN   CLITERAL(Color){170, 255, 238, 255}
-#define E_PINK   CLITERAL(Color){204,  68, 204, 255}
-#define E_GREEN  CLITERAL(Color){  0, 204,  85, 255}
-#define E_BLUE   CLITERAL(Color){  0,   0, 170, 255}
-#define E_YELLOW CLITERAL(Color){238, 238, 119, 255}
-#define E_BEIGE  CLITERAL(Color){221, 136,  85, 255}
-#define E_BROWN  CLITERAL(Color){102,  68,   0, 255}
-#define E_PEACH  CLITERAL(Color){255, 119, 119, 255}
-#define E_DRGRAY CLITERAL(Color){ 51,  51,  51, 255}
-#define E_MDGRAY CLITERAL(Color){119, 119, 119, 255}
-#define E_LIME   CLITERAL(Color){170, 255, 102, 255}
-#define E_LTBLUE CLITERAL(Color){  0, 136, 255, 255}
-#define E_LTGRAY CLITERAL(Color){187, 187, 187, 255}
+struct RayColor {
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+    unsigned char a;
+};
 
-extern struct Color colors[16];
+extern struct RayColor colors[16];
 
 int readInput(void);
 bool shouldCloseWindow(void);
