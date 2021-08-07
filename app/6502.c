058fe0075b3110435015f6119e9ea089d72e8c0e
Rafael Delboni <rafadelboni@gmail.com>
2021-08-07 12:52:50 -0300

    fix: Using zero when no fps arg

--- app/6502.c
+++ app/6502.c
@@ -1,14 +1,15 @@
+#include "cpu.h"
+#include "ray.h"
+#include "time.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "ray.h"
-#include "cpu.h"
-#include <time.h>    
 
 const int scale = 20;
 const int screenWidth = 32;
 const int screenHeight = 32;
 
-int initSimulator (struct CPU * cpu, struct BUS * bus, int initial_address, char * programPath) {
+int initSimulator(struct CPU *cpu, struct BUS *bus, int initial_address,
+                  char *programPath) {
   srand(time(NULL));
   writeBus(0xFFFC, initial_address & 0xff, bus);
   writeBus(0xFFFD, (initial_address >> 8), bus);
@@ -16,9 +17,7 @@ int initSimulator (struct CPU * cpu, struct BUS * bus, int initial_address, char
   return writeProgramToBus(programPath, initial_address, bus);
 }
 
-void generateRandomByte(struct BUS *bus) {
-  writeBus(0xFE, rand() % 255, bus);
-}
+void generateRandomByte(struct BUS *bus) { writeBus(0xFE, rand() % 255, bus); }
 
 void handleInput(struct BUS *bus) {
   int input = readInput();
@@ -28,9 +27,8 @@ void handleInput(struct BUS *bus) {
   }
 }
 
-void loop(struct CPU * cpu, struct BUS * bus) {
-  while (shouldCloseWindow())
-  {
+void loop(struct CPU *cpu, struct BUS *bus) {
+  while (shouldCloseWindow()) {
     generateRandomByte(bus);
     handleInput(bus);
     drawScreen(scale, bus->display);
@@ -41,9 +39,10 @@ void loop(struct CPU * cpu, struct BUS * bus) {
   }
 }
 
-int main(int argc, char **argv)
-{
-  char * programPath = NULL;
+int main(int argc, char **argv) {
+  char *programPath = NULL;
+  int fps = 0;
+
   if (argc > 1) {
     programPath = argv[1];
   } else {
@@ -51,13 +50,16 @@ int main(int argc, char **argv)
     return 1;
   }
 
+  if (argc > 2) {
+    fps = strtol(argv[2], NULL, 10);
+  }
+
   struct CPU cpu;
   struct BUS bus = initializeBus();
 
-  if(initSimulator(&cpu, &bus, 0x0600, programPath) != 0)
+  if (initSimulator(&cpu, &bus, 0x0600, programPath) != 0)
     return 1;
 
-  int fps = strtol(argv[2], NULL, 10);
   printf("Considering fps as %i\n", fps);
 
   initWindow(scale, screenWidth, screenHeight, fps);
